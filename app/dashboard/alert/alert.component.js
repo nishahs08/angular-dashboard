4cdd31886baf6873332e96101526c4b84d43a143
Nisha Singh <nishahs08@outlook.in>
2017-09-27 23:22:03 +0530

    Filters added along with repeaters

--- app/dashboard/alert/alert.component.js
+++ app/dashboard/alert/alert.component.js
@@ -3,5 +3,11 @@ angular
     .component('dashAlert', {
         templateUrl: 'app/dashboard/alert/alert.template.html',
         controller: function DashAlertController($scope) {
+            this.query = '';
+            this.names = [
+                'Himanshu',
+                'Nisha',
+                'Gabbar'
+            ];
         }
     })
