6ccd239726b5ca799644454d16148563be56775f
Nisha Singh <nishahs08@outlook.in>
2017-09-28 12:09:24 +0530

    Dynamic table data

--- app/dashboard/alert/alert.component.js
+++ app/dashboard/alert/alert.component.js
@@ -3,11 +3,40 @@ angular
     .component('dashAlert', {
         templateUrl: 'app/dashboard/alert/alert.template.html',
         controller: function DashAlertController($scope) {
-            this.query = '';
-            this.names = [
-                'Himanshu',
-                'Nisha',
-                'Gabbar'
-            ];
+            this.tableData = [
+                {
+                    alert: 3305029,
+                    asset_name: 'PAC',
+                    data_point: 'STN1',
+                    properties: 'Humidity',
+                    description: 'Hight',
+                    current_value: 33100,
+                    is_selection: true,
+                    reaction_plan: '123123',
+                    acknowledge: true
+                },
+                {
+                    alert: 1121233,
+                    asset_name: 'PAC2',
+                    data_point: 'STN2',
+                    properties: 'Rigidity',
+                    description: 'Low',
+                    current_value: 110011,
+                    is_selection: false,
+                    reaction_plan: '0128301',
+                    acknowledge: false
+                },
+                {
+                    alert: 3305029,
+                    asset_name: 'PAC',
+                    data_point: 'STN1',
+                    properties: 'Humidity',
+                    description: 'Hight',
+                    current_value: 33100,
+                    is_selection: true,
+                    reaction_plan: '123123',
+                    acknowledge: true
+                }
+            ]
         }
     })
